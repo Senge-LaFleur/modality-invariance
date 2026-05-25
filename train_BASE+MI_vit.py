6a573c5b68021acefb567b22dedf3f50adf17b53
Senge-LaFleur <asosenge@gmail.com>
2026-05-25 01:23:16 +0100

    update

--- train_BASE+MI_vit.py
+++ train_BASE+MI_vit.py
@@ -96,11 +96,11 @@ CFG = {
     'num_skin_types': 6,
 
     'batch_size': 32,
-    'num_epochs': 50,          # adjust as needed
+    'num_epochs': 1,          # adjust as needed
     'lr': 1e-4,
     'min_lr': 1e-6,
     'weight_decay': 1e-4,
-    'warmup_epochs': 10,
+    'warmup_epochs': 1,
     'aug_probability': 0.85,
 
     # MI loss weight
