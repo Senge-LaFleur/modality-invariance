d70e6a829e40a1f59d9a82b960e83d468d91c5db
Senge-LaFleur <asosenge@gmail.com>
2026-06-13 11:26:46 +0100

    update

--- train_Modality_Invariance_vit.py
+++ train_Modality_Invariance_vit.py
@@ -128,7 +128,7 @@ CFG = {
     'results_dir':  WORK_ROOT / 'results_Modality_Invariance_vit',
 
     'backbone':        'vit_small_patch16_224',
-    'embed_dim':       768,
+    'embed_dim':       512,
     'img_size':        224,
     'num_classes':     3,      # melanoma / nevus / basal cell carcinoma
     'num_skin_types':  6,
